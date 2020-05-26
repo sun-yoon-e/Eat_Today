52a72e9bcfd852d051b105966a96737a6c54d681
sun-yoon-e <shinee252525@icloud.com>
2020-05-26 23:46:51 +0900

    배경색 이쁘다

--- Term-project/Term-project.py
+++ Term-project/Term-project.py
@@ -1,7 +1,11 @@
 from tkinter import *
 
-window = Tk()
-window.title('오늘 뭐 먹지~?')
-canvas = Canvas(window, bg='light gray', width=600, height=800)
-canvas.pack()
-window.mainloop()
+class EatToday:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('오늘 뭐 먹지~?')
+        self.window.geometry('600x800')
+        self.window.configure(background='lemon chiffon')
+        self.window.mainloop()
+
+EatToday()
