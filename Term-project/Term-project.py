8a491bd63c566838f1e36e696a82434d8b19513a
sun-yoon-e <shinee252525@icloud.com>
2020-05-27 00:38:33 +0900

    리소스

--- Term-project/Term-project.py
+++ Term-project/Term-project.py
@@ -1,11 +1,16 @@
 from tkinter import *
 
+
 class EatToday:
     def __init__(self):
         self.window = Tk()
         self.window.title('오늘 뭐 먹지~?')
         self.window.geometry('600x800')
-        self.window.configure(background='lemon chiffon')
+        self.window.configure(background='RosyBrown1') #lemon chiffon thistle powder blue
         self.window.mainloop()
+
+        # 카테고리 별 이미지 찾기 (배민 꺼  쓰고싶당)
+
+
 
 EatToday()
