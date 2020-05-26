7330e6a9f1e43631639bc9ac255e65676343754c
sun-yoon-e <shinee252525@icloud.com>
2020-05-27 03:06:49 +0900

    UI 너무안이뻐!!!!!!!!

--- Term-project/Term-project.py
+++ Term-project/Term-project.py
@@ -6,12 +6,17 @@ class EatToday:
         self.window = Tk()
         self.window.title('오늘 뭐 먹지~?')
         self.window.geometry('600x800')
-        self.window.configure(background='lemon chiffon') #RosyBrown1 thistle powder blue
+        self.window.configure(bg='lemon chiffon') #RosyBrown1 thistle powder blue
 
-        self.fontstyle = font.Font(self.window, size=20, weight='bold', family='resources/font/BMJU_ttf_0.ttf')
+        self.font = font.Font(self.window, size=20, weight='bold', family='resources/font/BMJU_ttf_0.ttf')
+        self.font2 = font.Font(self.window, size=18, weight='bold', family='resources/font/BMJU_ttf_0.ttf')
+        self.font3 = font.Font(self.window, size=16, weight='bold', family='resources/font/BMJU_ttf_0.ttf')
         #self.Logo
 
         self.initMail()
+        self.initSearchListBox()
+        self.initInputLabel()
+        self.initSearchButton()
         self.setupButton()
 
 
@@ -20,22 +25,81 @@ class EatToday:
     def initMail(self):
         self.mailImage = PhotoImage(file='resources/image/gmail.png')
         self.mailButton = Button(self.window, image=self.mailImage, command=self.sendMail)
+        self.mailButton.place(x=450, y=30)
 
     def sendMail(self):
         pass
 
+    def initSearchListBox(self):
+        global SearchList
+        ListScrollbar = Scrollbar(self.window)
+        ListScrollbar.pack()
+        ListScrollbar.place(x=230, y=130)
+
+        SearchList = Listbox(self.window, font=self.font3, activestyle='dotbox', width=15, height=1, bd=12,
+                              cursor='heart', relief='ridge', yscrollcommand=ListScrollbar.set, fg='thistle4')
+
+        SearchList.insert(1, "가평군")
+        SearchList.insert(2, "고양시")
+        SearchList.insert(3, "과천시")
+        SearchList.insert(4, "광명시")
+        SearchList.insert(5, "광주시")
+        SearchList.insert(6, "구리시")
+        SearchList.insert(7, "군포시")
+        SearchList.insert(8, "김포시")
+        SearchList.insert(9, "남양주시")
+        SearchList.insert(10, "동두천시")
+        SearchList.insert(11, "부천시")
+        SearchList.insert(12, "성남시")
+        SearchList.insert(13, "수원시")
+        SearchList.insert(14, "시흥시")
+        SearchList.insert(15, "안산시")
+        SearchList.insert(16, "안성시")
+        SearchList.insert(17, "안양시")
+        SearchList.insert(18, "양주시")
+        SearchList.insert(19, "양평군")
+        SearchList.insert(20, "여주시")
+        SearchList.insert(21, "연천군")
+        SearchList.insert(22, "오산시")
+        SearchList.insert(23, "용인시")
+        SearchList.insert(24, "의왕시")
+        SearchList.insert(25, "의정부시")
+        SearchList.insert(26, "이천시")
+        SearchList.insert(27, "파주시")
+        SearchList.insert(28, "평택시")
+        SearchList.insert(29, "포천시")
+        SearchList.insert(30, "하남시")
+        SearchList.insert(31, "화성시")
+        SearchList.pack()
+        SearchList.place(x=25, y=130)
+        ListScrollbar.config(command=SearchList.yview)
+
+    def initInputLabel(self):
+        global InputLabel
+        InputLabel = Entry(self.window, font=self.font3, width=18, bd=12, relief='ridge')
+        InputLabel.pack()
+        InputLabel.place(x=265, y=130)
+
+    def initSearchButton(self):
+        SearchButton = Button(self.window, font=self.font2, text="검색", command=self.SearchButtonAction)
+        SearchButton.pack()
+        SearchButton.place(x=509, y=130)
+
+    def SearchButtonAction(self):
+        pass
+
     def setupButton(self):
-        self.Korea = Button(self.window, text="한식", font=self.fontstyle, command=self.pressedKr)
+        self.Korea = Button(self.window, text="한식", font=self.font, command=self.pressedKr)
         self.Korea.place(x=25, y=200)
-        self.China = Button(self.window, text="중식", font=self.fontstyle, command=self.pressedCn)
+        self.China = Button(self.window, text="중식", font=self.font, command=self.pressedCn)
         self.China.place(x=120, y=200)
-        self.Japan = Button(self.window, text="일식", font=self.fontstyle, command=self.pressedJp)
+        self.Japan = Button(self.window, text="일식", font=self.font, command=self.pressedJp)
         self.Japan.place(x=215, y=200)
-        self.Italy = Button(self.window, text="양식", font=self.fontstyle, command=self.pressedIt)
+        self.Italy = Button(self.window, text="양식", font=self.font, command=self.pressedIt)
         self.Italy.place(x=310, y=200)
-        self.Cafe = Button(self.window, text="카페", font=self.fontstyle, command=self.pressedCf)
+        self.Cafe = Button(self.window, text="카페", font=self.font, command=self.pressedCf)
         self.Cafe.place(x=405, y=200)
-        self.Famous = Button(self.window, text="맛집", font=self.fontstyle, command=self.pressedFm)
+        self.Famous = Button(self.window, text="맛집", font=self.font, command=self.pressedFm)
         self.Famous.place(x=500, y=200)
 
     def pressedKr(self):
