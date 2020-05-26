742fffc03972198d69e0c7232b1716172434da5e
sun-yoon-e <shinee252525@icloud.com>
2020-05-26 23:28:40 +0900

    Tkinter UI 뼈대

--- Term-project/Term-project.py
+++ Term-project/Term-project.py
@@ -2,6 +2,6 @@ from tkinter import *
 
 window = Tk()
 window.title('오늘 뭐 먹지~?')
-canvas = Canvas(window, bg='light gray', width=500, height=600)
+canvas = Canvas(window, bg='light gray', width=600, height=800)
 canvas.pack()
 window.mainloop()
