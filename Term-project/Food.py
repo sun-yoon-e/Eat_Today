5b956bd94f303ad58cda21c9571055a59789e915
JHJ-dev <wjs3662@naver.com>
2020-06-06 01:09:22 +0900

    food.py 정리

--- Term-project/Food.py
+++ Term-project/Food.py
@@ -11,33 +11,37 @@ FamousList = []
 AllList =[]     #그래프용 모든 카테고리 리스트
 SearchList = [] #검색용 리스트
 
-KoreaKEY = "/Genrestrtsoup?KEY=eacb09e4cc1e4b5f9bf7f14ebe87291b"
-ChinaKEY = "/Genrestrtchifood?KEY=062afd00409748bfbeedbd63d2851b62"
-JapanKEY = "/Genrestrtjpnfood?KEY=55e63a8c30644642b07f671996903252"
-ItalyKEY = "/Genrestrtfastfood?KEY=308a1836ded941e69da26b59698c3c68"
-CafeKEY = "/Genrestrtcate?KEY=46c5a83322734a8b83ae785069ca6619"
-FamousKEY = "/PlaceThatDoATasteyFoodSt?KEY=de547a5cf35444bb9e49043ce00f4115"
+def URLbuilder(Category):   #카테고리별 URL
+    if Category == "Korea":
+        KEY = "/Genrestrtsoup?KEY=eacb09e4cc1e4b5f9bf7f14ebe87291b"
+    elif Category == "China":
+        KEY = "/Genrestrtchifood?KEY=062afd00409748bfbeedbd63d2851b62"
+    elif Category == "Japan":
+        KEY = "/Genrestrtjpnfood?KEY=55e63a8c30644642b07f671996903252"
+    elif Category == "Italy":
+        KEY = "/Genrestrtfastfood?KEY=308a1836ded941e69da26b59698c3c68"
+    elif Category == "Cafe":
+        KEY = "/Genrestrtcate?KEY=46c5a83322734a8b83ae785069ca6619"
+    elif Category == "Famous":
+        KEY = "/PlaceThatDoATasteyFoodSt?KEY=de547a5cf35444bb9e49043ce00f4115"
 
-City = "&SIGUN_NM="
-SIGUN_NM = ['가평군', '고양시', '과천시', '광명시', '광주시', '구리시', '군포시', '김포시',
-            '남양주시', '동두천시', '부천시', '성남시', '수원시', '시흥시', '안산시', '안성시',
-            '안양시', '양주시', '양평군', '여주시', '연천군', '오산시', '용인시', '의왕시',
-            '의정부시', '이천시', '파주시', '평택시', '포천시', '하남시', '화성시']
+    URLrequest(Category, KEY)
 
-def URLrequest(Category):   #카테고리별 파싱
+def URLrequest(Category, KEY):   #카테고리별 파싱
     conn = http.client.HTTPSConnection("openapi.gg.go.kr")
-    conn.request("GET", Category)
+    conn.request("GET", KEY)
     req = conn.getresponse()
 
     if req.status == 200:
         temp = req.read().decode('utf-8')
-        #print(temp)
         print("Data Downloading Complete!")
-        return putXmlToList(temp)
+        if Category == "Famous":
+            return XmlToList2(Category, temp)
+        else: return XmlToList1(Category, temp)
     else:
         print("OpenAPI request Failed!")
 
-def putXmlToList(xml):  #xml->카테고리별 리스트로
+def XmlToList1(Category, xml):  # xml → 카테고리별(맛집 외) 리스트로
     tree = ElementTree.fromstring(xml)
 
     for restaurant in tree.findall('./row'):
@@ -49,16 +53,41 @@ def putXmlToList(xml):  #xml->카테고리별 리스트로
         Lat = restaurant.find('REFINE_WGS84_LAT')  # 위도(22)
         Long = restaurant.find('REFINE_WGS84_LOGT')  # 경도(23)
 
-        KoreaList.append([City.text, Name.text, RoadAdress.text, Address.text, Post.text, Lat.text, Long.text])
+        if Category == "Korea":
+            KoreaList.append([City.text, Name.text, RoadAdress.text, Address.text, Post.text, Lat.text, Long.text])
+        elif Category == "China":
+            ChinaList.append([City.text, Name.text, RoadAdress.text, Address.text, Post.text, Lat.text, Long.text])
+        elif Category == "Japan":
+            JapanList.append([City.text, Name.text, RoadAdress.text, Address.text, Post.text, Lat.text, Long.text])
+        elif Category == "Italy":
+            ItalyList.append([City.text, Name.text, RoadAdress.text, Address.text, Post.text, Lat.text, Long.text])
+        elif Category == "Cafe":
+            CafeList.append([City.text, Name.text, RoadAdress.text, Address.text, Post.text, Lat.text, Long.text])
 
-        # print(City.text)
-        # print(Name.text)
-        # print(RoadAdress.text)
-        # print(Address.text)
-        # print(Post.text)
-        # print(Lat.text)
-        # print(Long.text)
+def XmlToList2(Category, xml):  # xml → 맛집 리스트로
+    tree = ElementTree.fromstring(xml)
 
-    print(KoreaList)
+    for restaurant in tree.findall('./row'):
+        City = restaurant.find('SIGUN_NM')  # 시군명(1)
+        Name = restaurant.find('RESTRT_NM')  # 사업장명(3)
+        RoadAdress = restaurant.find('REFINE_ROADNM_ADDR')  # 도로명 주소(19)
+        Address = restaurant.find('REFINE_LOTNO_ADDR')  # 지번 주소(20)
+        Post = restaurant.find('REFINE_ZIP_CD')  # 우편 번호(21)
+        Lat = restaurant.find('REFINE_WGS84_LAT')  # 위도(22)
+        Long = restaurant.find('REFINE_WGS84_LOGT')  # 경도(23)
 
-URLrequest(KoreaKEY)
+        FamousList.append([City.text, Name.text, RoadAdress.text, Address.text, Post.text, Lat.text, Long.text])
+
+URLbuilder("Korea")
+URLbuilder("China")
+URLbuilder("Japan")
+URLbuilder("Italy")
+URLbuilder("Cafe")
+URLbuilder("Famous")
+
+print(KoreaList)
+print(ChinaList)
+print(JapanList)
+print(ItalyList)
+print(CafeList)
+print(FamousList)
