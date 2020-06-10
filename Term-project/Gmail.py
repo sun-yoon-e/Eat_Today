641303e6d29a538f9df7c445a19e500e0edcc415
sun-yoon-e <shinee252525@icloud.com>
2020-06-10 18:40:07 +0900

    메일 고치기

--- Term-project/Gmail.py
+++ Term-project/Gmail.py
@@ -4,25 +4,28 @@ from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-host = "smtp.gmail.com"                     #Gmail STMP 서버 주소
+host = "smtp.gmail.com" # Gmail STMP 서버 주소.
 port = "587"
+
 #htmlFileName = "Eat_Today_Map.html"
 
-senderAddr = "shinee2525@gmail.com"         # 보내는 사람 email 주소
-recipientAddr = "shinee252525@icloud.com"   # 받는 사람 email 주소
 
 def sendMail(MailList):
+    global host, port
+
     html = ""
     title = 'Eat_Today 정보 전송'
+    senderAddr = "shinee2525@gmail.com"         # 보내는 사람 email 주소
+    recipientAddr = "shinee252525@icloud.com"  # 받는 사람 email 주소
     passwd = 'tjs*951753'
 
     for informaion in MailList:
         for text in informaion:
-            html = html + text
+            html += text
     print(html)
 
     msg = MIMEBase("multipart", "alternative")
-    msg = MIMEMultipart('alternative')
+    #msg = MIMEMultipart('alternative')
     msg['Subject'] = title
     msg['From'] = senderAddr
     msg['To'] = recipientAddr
